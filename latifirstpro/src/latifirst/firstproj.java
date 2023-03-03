0b59dfc76317d81095fd8c699fbf4933f1c98ff7
latera <lateratibebu@gmai.com>
2023-03-03 16:08:24 -0700

    first eclipse

---
+++ latifirstpro/src/latifirst/firstproj.java
@@ -0,0 +1,16 @@
+package latifirst;
+
+public class firstproj {
+
+	public firstproj() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("latera");
+
+
+	}
+
+}
